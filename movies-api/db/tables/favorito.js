d174d0527fb93ae02c2fa973005c0b2967265ea0
Diego Fleitas <diego.fleitas68@gmail.com>
2021-08-05 01:09:33 -0300

    :recycle: refactor favorito structure to include themoviedb data

--- movies-api/db/tables/favorito.js
+++ movies-api/db/tables/favorito.js
@@ -1,6 +1,23 @@
 class Favorito {
-    constructor(suggestionForTodayScore) {
-        this.suggestionForTodayScore = suggestionForTodayScore;
+    constructor(userEmail, adult, backdropPath, genreIds, id, originalLanguage, originalTitle,
+                overview, popularity, posterPath, releaseDate, title, video, voteAverage, voteCount
+    ) {
+        this.suggestionForTodayScore = (Math.random() * 99).toFixed(2);
+        this.userEmail = userEmail;
+        this.adult = adult,
+        this.backdropPath = backdropPath,
+        this.genreIds = genreIds,
+        this.id = id,
+        this.originalLanguage = originalLanguage,
+        this.originalTitle = originalTitle,
+        this.overview = overview,
+        this.popularity = popularity,
+        this.posterPath = posterPath,
+        this.releaseDate = releaseDate,
+        this.title = title,
+        this.video = video,
+        this.voteAverage = voteAverage,
+        this.voteCount = voteCount
     }
 }
 
