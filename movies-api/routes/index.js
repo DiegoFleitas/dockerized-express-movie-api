57d018f0ee6469f68095e76a50406d9fab6a2d0f
Diego Fleitas <diego.fleitas68@gmail.com>
2021-08-05 03:12:36 -0300

    :art: fix spacing

--- movies-api/routes/index.js
+++ movies-api/routes/index.js
@@ -6,42 +6,42 @@ var User = require('../db/tables/user');
 var Favorito = require('../db/tables/favorito');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 router.get("/test/favoritos/add", function (req, res) {
-  const newFavorito = new Favorito('ruben.rada@gmail.com', false,
-      null,
-      [35, 18],
-      63066,
-      "es",
-      "25 Watts",
-      "24 hours in the life of three street youths in Montevideo. Three teenage guys try to figure out what they're " +
-      "supposed to be doing with their lives in this drama from Uruguay that puts the emphasis on character over " +
-      "narrative.",
-      1.193,
-      "/cSuy4RgmLZmfZXyMAyDA07PkBPo.jpg",
-      "2001-01-01",
-      "25 Watts",
-      false,
-      6.7,
-      20
-  );
-  db.insert('favoritos', newFavorito);
-  res.render('index', { title: 'success' });
+    const newFavorito = new Favorito('ruben.rada@gmail.com', false,
+        null,
+        [35, 18],
+        63066,
+        "es",
+        "25 Watts",
+        "24 hours in the life of three street youths in Montevideo. Three teenage guys try to figure out what they're " +
+        "supposed to be doing with their lives in this drama from Uruguay that puts the emphasis on character over " +
+        "narrative.",
+        1.193,
+        "/cSuy4RgmLZmfZXyMAyDA07PkBPo.jpg",
+        "2001-01-01",
+        "25 Watts",
+        false,
+        6.7,
+        20
+    );
+    db.insert('favoritos', newFavorito);
+    res.render('index', {title: 'success'});
 });
 
 router.get("/test/users/add", function (req, res) {
-  const newUser = new User('diego.forlan@gmail.com', 'Diego', 'Forlan', 'cachavacha10');
-  db.insert('users', newUser);
-  res.render('index', { title: 'success' });
+    const newUser = new User('diego.forlan@gmail.com', 'Diego', 'Forlan', 'cachavacha10');
+    db.insert('users', newUser);
+    res.render('index', {title: 'success'});
 });
 
 router.get("/test/users/remove", function (req, res) {
-  const userToDelete = new User('ruben.rada@gmail.com', 'Ruben', 'Rada', 'nillantonipena');
-  db.delete('users', userToDelete);
-  res.render('index', { title: 'success' });
+    const userToDelete = new User('ruben.rada@gmail.com', 'Ruben', 'Rada', 'nillantonipena');
+    db.delete('users', userToDelete);
+    res.render('index', {title: 'success'});
 });
 
 module.exports = router;
