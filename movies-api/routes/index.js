a9b9e891bd89587245dc9d8d473f68450005a310
Diego Fleitas <diego.fleitas68@gmail.com>
2021-08-05 01:11:58 -0300

    :clown_face: mock up adding/deleting users & adding favoritos

--- movies-api/routes/index.js
+++ movies-api/routes/index.js
@@ -11,19 +11,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/test/favoritos/add", function (req, res) {
-  const newFavorito = new Favorito('1');
+  const newFavorito = new Favorito('ruben.rada@gmail.com', false,
+      null,
+      [35, 18],
+      63066,
+      "es",
+      "25 Watts",
+      "24 hours in the life of three street youths in Montevideo. Three teenage guys try to figure out what they're " +
+      "supposed to be doing with their lives in this drama from Uruguay that puts the emphasis on character over " +
+      "narrative.",
+      1.193,
+      "/cSuy4RgmLZmfZXyMAyDA07PkBPo.jpg",
+      "2001-01-01",
+      "25 Watts",
+      false,
+      6.7,
+      20
+  );
   db.insert('favoritos', newFavorito);
   res.render('index', { title: 'success' });
 });
 
 router.get("/test/users/add", function (req, res) {
-  const newUser = new User('mail', 'firstname', 'lastname', 'password');
+  const newUser = new User('diego.forlan@gmail.com', 'Diego', 'Forlan', 'cachavacha10');
   db.insert('users', newUser);
   res.render('index', { title: 'success' });
 });
 
 router.get("/test/users/remove", function (req, res) {
-  const userToDelete = new User('mail2', 'firstname', 'lastname', 'password');
+  const userToDelete = new User('ruben.rada@gmail.com', 'Ruben', 'Rada', 'nillantonipena');
   db.delete('users', userToDelete);
   res.render('index', { title: 'success' });
 });
