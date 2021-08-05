5b65b2313545bae3a7e9625395a6755d8956b9d8
Diego Fleitas <diego.fleitas68@gmail.com>
2021-08-05 18:42:54 -0300

    :art: fix spacing

--- movies-api/routes/users.js
+++ movies-api/routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var db = require('../db/database.js');
+var User = require('../db/tables/user');
+var Favorito = require('../db/tables/favorito');
+
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 /* register a new user. */
@@ -11,18 +15,18 @@ router.post('/register', function(req, res, next) {
 });
 
 /* authenticate user. */
-router.post('/login', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 /* add movie to user favorites. */
-router.post('/add/favorite', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/add/favorite', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 /* fetch user favorite movies. */
-router.get('/fetch/favorite', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/fetch/favorite', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
