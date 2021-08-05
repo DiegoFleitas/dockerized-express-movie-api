63645e7838f84fae98afa9a9c1481317262922a5
Diego Fleitas <diego.fleitas68@gmail.com>
2021-08-05 19:45:54 -0300

    :sparkles: implement register user

--- movies-api/routes/users.js
+++ movies-api/routes/users.js
@@ -10,8 +10,28 @@ router.get('/', function (req, res, next) {
 });
 
 /* register a new user. */
-router.post('/register', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function (req, res, next) {
+    try {
+        const email = req.body.email;
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const password = req.body.password;
+        if (email && firstName && lastName && password) {
+            const user = new User(email, firstName, lastName, password);
+            const exists = db.where('users', 'email', email);
+            console.log('exists', exists);
+            if (exists) {
+                res.status(403).send('user already exists.');
+            } else {
+                db.insert('users', user);
+                res.status(200).send('user registered.');
+            }
+        }
+        res.status(400).send('user data missing.');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('server error.');
+    }
 });
 
 /* authenticate user. */
